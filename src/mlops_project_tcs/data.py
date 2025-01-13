d6257b5b6761559aa2988944cdfe91005c49bcfb
gaston <tamstamsthomas@gmail.com>
2025-01-13 11:18:18 +0100

    add: typer cli for download and preprocess

--- src/mlops_project_tcs/data.py
+++ src/mlops_project_tcs/data.py
@@ -1,5 +1,5 @@
 from loguru import logger
-from typing import Union
+from typing import Union, Annotated
 from pathlib import Path
 import os
 import shutil
@@ -14,11 +14,10 @@ from crop_img import CropExtremePoints
 app = typer.Typer()
 
 def get_kaggle_dataset(
-        kaggle_handle: Union[Path, str],
+        kaggle_handle: str,
         raw_data_dir: Union[Path, str]
 ) -> None:
     """Download dataset using kagglehub and place it in the specified raw data folder."""
-    
     try:
         # Access data from kaggle
         downloaded_path = kagglehub.dataset_download(kaggle_handle)
@@ -26,9 +25,17 @@ def get_kaggle_dataset(
     except Exception as e:
         logger.error(f"Failed to download dataset from Kaggle: {e}")
         return
+    
+    # Removing double data (for some reason this kaggle dataset contains double up on the dataset)
+    if kaggle_handle == "navoneel/brain-mri-images-for-brain-tumor-detection":
+        downloaded_path = Path(downloaded_path)
+        brain_tumor_dataset_path = downloaded_path / "brain_tumor_dataset"
+        if brain_tumor_dataset_path.exists() and brain_tumor_dataset_path.is_dir():
+            shutil.rmtree(brain_tumor_dataset_path)
+            logger.info(f"Removed directory: {brain_tumor_dataset_path}")
 
     # Ensure the target folder exists
-    raw_data_dir = Path(raw_data_dir)    
+    raw_data_dir = Path(raw_data_dir)
     raw_data_dir.mkdir(parents=True, exist_ok=True)
 
     # Copy files to raw directory
@@ -77,19 +84,26 @@ class MyDataset(Dataset):
             output_path = label_folder / Path(img_path).name
             processed_image_pil = transforms.ToPILImage()(processed_image)  # Convert tensor to PIL Image
             processed_image_pil.save(output_path)
-            print(f"Processed and saved: {output_path}")
+            logger.info(f"Processed and saved: {output_path}")
+
+@app.command()
+def download(
+    kaggle_handle: Annotated[str, typer.Option("--kaggle_handle", "-kh", help="Kaggle handle pointing to download location")] = "navoneel/brain-mri-images-for-brain-tumor-detection",
+    raw_data_dir: Annotated[str, typer.Option("--output_dir", "-o", help="Output directory of the downloaded Kaggle data")] = "data/raw",
+) -> None:
+    logger.info("Downloading and preparing raw data...")
+    get_kaggle_dataset(kaggle_handle, raw_data_dir)
+
 
 @app.command()
 def preprocess(
-    kaggle_handle: str = "navoneel/brain-mri-images-for-brain-tumor-detection",
-    raw_data_dir: str = "data/raw"
+    raw_data_dir: Annotated[str, typer.Option("--indir", "-i", help="Input directory of raw data")] = "data/raw",
+    output_folder: Annotated[str, typer.Option("--outdir", "-o", help="Output directory of preprocessed files")] = "data/processed"
 ) -> None:
-    print("Downloading and preparing raw data...")
-    get_kaggle_dataset(kaggle_handle, raw_data_dir)
+    logger.info("Preprocessing data...")
+    dataset = MyDataset(Path(raw_data_dir))
+    dataset.preprocess(Path(output_folder))
 
-    #print("Preprocessing data...")
-    #dataset = MyDataset(raw_data_path)
-    #dataset.preprocess(output_folder)
 
 if __name__ == "__main__":
     app()
