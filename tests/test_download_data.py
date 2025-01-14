9efcc5bc641a440ff893ebd20a8f46c460942000
gaston <tamstamsthomas@gmail.com>
2025-01-14 13:26:18 +0100

    add: fixture to clean up files

--- tests/test_download_data.py
+++ tests/test_download_data.py
@@ -1,5 +1,6 @@
 import pytest
 from pathlib import Path
+import shutil
 from src.mlops_project_tcs.data import download
 
 from tests import _PATH_DATA
@@ -7,8 +8,21 @@ from tests import _PATH_DATA
 # Define the paths to the raw and processed data
 RAW_DATA_PATH = _PATH_DATA / "raw"
 
+
+@pytest.fixture
+def setup_cleaning_fixture():
+    # Create raw data directories
+    for category in ["yes", "no"]:
+        category_path = RAW_DATA_PATH / category
+        category_path.mkdir(parents=True, exist_ok=True)
+    yield
+    # Cleanup after tests
+    for category in ["yes", "no"]:
+        category_path = RAW_DATA_PATH / category
+        shutil.rmtree(category_path)
+
 @pytest.mark.parametrize("category", ["yes", "no"])
-def test_raw_data_loading(category):
+def test_raw_data_loading(setup_cleaning_fixture, category):
     """Test that raw data is downloaded."""
     download()
     category_path = RAW_DATA_PATH / category
