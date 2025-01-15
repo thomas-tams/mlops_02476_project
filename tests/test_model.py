62aea8a0f906416ff0c1ad42ecf1b161c6d5c5cc
gaston <tamstamsthomas@gmail.com>
2025-01-15 15:49:28 +0100

    fix: remove data and dataloader mocks, not needed after pytorch lightingin datamodule implementation

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,45 +1,24 @@
 import torch
 from mlops_project_tcs.model import VGG16Classifier
-from hydra import initialize, compose
-from unittest.mock import patch, MagicMock
-import pytest
+import hydra
 
 
-@pytest.fixture
-def mock_setup_dataloaders():
-    with (
-        patch("mlops_project_tcs.data.transforms.Compose") as mock_transforms_compose,
-        patch("mlops_project_tcs.data.datasets.ImageFolder") as mock_image_folder,
-        patch("mlops_project_tcs.data.random_split") as mock_random_split,
-        patch("mlops_project_tcs.data.DataLoader") as mock_dataloader,
-    ):
-        # Mock the transformations
-        mock_transforms_compose.return_value = MagicMock()
-
-        # Mock the ImageFolder dataset
-        mock_image_folder_instance = MagicMock()
-        mock_image_folder_instance.__len__.return_value = 100
-        mock_image_folder.return_value = mock_image_folder_instance
-
-        # Mock the random_split function
-        mock_train_dataset = MagicMock()
-        mock_val_dataset = MagicMock()
-        mock_random_split.return_value = (mock_train_dataset, mock_val_dataset)
-
-        # Mock the DataLoader
-        mock_train_loader = MagicMock()
-        mock_val_loader = MagicMock()
-        mock_dataloader.side_effect = [mock_train_loader, mock_val_loader]
-
-        yield mock_train_loader, mock_val_loader
-
-
-def test_vgg16_classifier_output_shape(mock_setup_dataloaders):
+def test_vgg16_classifier_output_shape():
     """Test that VGG16Classifier produces the correct output shape."""
-    with initialize(config_path="../src/mlops_project_tcs/config", version_base="1.3"):
-        config = compose(config_name="test_config.yaml")
+    with hydra.initialize(config_path="../src/mlops_project_tcs/config", version_base="1.3"):
+        config = hydra.compose(config_name="test_config.yaml")
 
-    model = VGG16Classifier(config=config)
+    model = VGG16Classifier(
+        input_size=config.experiment.model["input_size"],
+        hidden_size=config.experiment.model["hidden_size"],
+        num_classes=config.experiment.dataset["num_classes"],
+        dropout_p=config.experiment.model["dropout_p"],
+        criterion=hydra.utils.instantiate(config.experiment.model.loss_fn),
+    )
+
+    model.configure_optimizers(
+        optimizer=hydra.utils.instantiate(config.experiment.hyperparameter.optimizer, params=model.parameters())
+    )
 
     # Create a dummy input tensor of shape (4, 3, 224, 224)
     input_tensor = torch.randn(4, 3, 224, 224)
